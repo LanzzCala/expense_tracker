2add3095682443bc6e5ea6b6d2f094d3d7e014d1
Lanzz Cala <lanzzcala@gmail.com>
2025-03-27 20:15:42 -0700

    Clean up code to be able to print dictionary

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,12 +10,15 @@ def get_expense_and_amount():
             item_amount = (input("Enter your amount: $"))
             date_added = date.today().strftime(" %Y-%m-%d")
             expense_list[item_id] = str({item_description : "$"+ item_amount}) + date_added
+            print(f"Expense successfully added. (ID: {item_id})")
             item_id += 1
         else:
             break
     return expense_list
 
-
-test = get_expense_and_amount()
-print (test)
+def display_expense_list(expense_list):
+    print (expense_list)
     
+
+expense_list = get_expense_and_amount()
+print(expense_list)
